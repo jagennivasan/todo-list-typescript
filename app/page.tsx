1926b8416831aee53a81b61710a45dfa92e68d1f
jagennivasan <professorjagan18@gmail.com>
2024-04-19 13:16:40 +0530

    adding todoitem components to page.ts

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
-import styles from "./page.module.css";
+import ToDoItem from "@/components/TodoItem/ToDoItem";
 
 export default function Home() {
-  return (
-  <div className={styles.container}>
-    hello world
-    </div>);
+  return <ToDoItem />;
 }
