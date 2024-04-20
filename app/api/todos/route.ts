1dce180e504f1149e8037b3fd8a40c78afc17ea7
jagennivasan <professorjagan18@gmail.com>
2024-04-20 16:18:25 +0530

    route.ts file created and functions are added

---
+++ app/api/todos/route.ts
@@ -0,0 +1,24 @@
+import { connectDB } from "@/lib/mongoDB";
+import ToDo from "@/models/ToDo";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req:NextRequest){
+    const {todos} = await req.json();
+    await connectDB();
+   await ToDo.create({todos});
+   return NextResponse.json({message:"Todo created"},{status:201})
+
+}
+export async function GET(){
+    await connectDB();
+    const todos = await ToDo.find();
+    return NextResponse.json({todos});
+}
+
+export async function DELETE(req:NextRequest){
+    const id = req.nextUrl.searchParams.get("id");
+  await connectDB();
+  await ToDo.findByIdAndDelete(id);
+  return NextResponse.json({ message: "Title Deleted" }, { status: 200 });
+
+}
