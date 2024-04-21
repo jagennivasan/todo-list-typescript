8201d692e3ff6e038a40e3f190a93a98ea2b4d79
jagennivasan <professorjagan18@gmail.com>
2024-04-21 12:25:31 +0530

    additem code completed

--- components/AddItem/Additem.tsx
+++ components/AddItem/Additem.tsx
@@ -1,19 +1,53 @@
+"use client"
+
+import { FormEvent, useState } from "react";
+import { useRouter } from "next/navigation";
+
 import Styles from "./Additem.module.css";
 import { IoAddOutline } from "react-icons/io5";
 export default function AddItem() {
+  const [todos, setTodos] = useState("");
+  const router = useRouter();
+
+  const handelSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!todos) {
+      alert("title is required");
+    }
+    try {
+      const res = await fetch("/api/todos", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ todos }),
+      });
+      if (res.ok) {
+        router.refresh();
+        setTodos("");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className={Styles.addContainer}>
-      <div className={Styles.addFields}>
+      <form onSubmit={handelSubmit} className={Styles.addFields}>
         <input
           placeholder="write something"
           type="text"
+          onChange={(e) => {
+            setTodos(e.target.value);
+          }}
           className={Styles.input}
         />
         <button className={Styles.addBtn}>
           <IoAddOutline size={25} />
           Add Item
         </button>
-      </div>
+      </form>
     </div>
   );
 }
