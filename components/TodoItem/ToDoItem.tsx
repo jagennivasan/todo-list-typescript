6c4e5adee0ed362612877ed9661ce96ac138d914
jagennivasan <professorjagan18@gmail.com>
2024-04-21 12:26:41 +0530

    to item code under developement

--- components/TodoItem/ToDoItem.tsx
+++ components/TodoItem/ToDoItem.tsx
@@ -1,12 +1,42 @@
 import RemoveBtn from "../RemoveBtn/RemoveBtn"
 import Styles from "./TodoItem.module.css"
-export default function ToDoItem(){
+
+
+interface Todo {
+    _id: string;
+    todos: string;
+  }
+const getTodos = async (): Promise<{todos:Todo[]}> => {
+    const apiUrl: string = process.env.NEXTAPI_URL || "";
+    try {
+      const res = await fetch(`${apiUrl}/api/todos`, {
+        cache: "no-store",
+      });
+      if (!res.ok) {
+        throw new Error("Failed to fetch Todo items");
+      }
+      return res.json();
+    } catch (error) {
+      console.error("Error", error);
+      return null;
+    }
+  };
+
+export default async function ToDoItem(){
+    const { todos } = await getTodos();
+
     return(
+        <>
+        {todos.map((t)=>(
+
+      
         <div className={Styles.todoContainer}>
-        <h1>Todo items</h1> 
+        <h1>{t.todos}</h1> 
         <div>
             <RemoveBtn/>
         </div>
         </div>
+          ))}
+        </>
     )
 }
